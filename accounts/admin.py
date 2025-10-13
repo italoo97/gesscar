c598c712d0ccf0950de490dd20eec033afaa232d
italoo97 <italohugodf39@gmail.com>
2025-10-13 12:54:59 -0300

    instalado o whitenoise

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Contact
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['user', 'first_name', 'surname', 'email', 'phone']
+
+@admin.register(Contact)
+class ProfileContactAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'whatsapp', 'enterprise', 'instagram', 'website']
